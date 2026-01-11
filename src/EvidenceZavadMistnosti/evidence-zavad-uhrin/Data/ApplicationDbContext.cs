c2c4df6235339f566cc15d3a90d599b32a25583d
Patrik Uhrin <uhrinpatrik@sssvt.cz>
2026-01-11 18:18:18 +0100

    blbnou mi balicky a mysql server phpmyadmin

---
+++ src/EvidenceZavadMistnosti/evidence-zavad-uhrin/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.EntityFrameworkCore;
+
+namespace evidence_zavad_uhrin.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        //public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        //{
+        //}
+    }
+}
