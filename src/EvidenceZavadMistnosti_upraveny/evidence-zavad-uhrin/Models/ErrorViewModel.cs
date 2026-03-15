aa929d14dd5006efe1893dab494387497f5031cd
Patrik Uhrin <UhrinPatrik@sssvt.cz>
2026-03-15 19:15:15 +0100

    Uhrin - Final

--- src/EvidenceZavadMistnosti_upraveny/evidence-zavad-uhrin/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace evidence_zavad_uhrin.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
